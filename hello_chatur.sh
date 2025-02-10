0514b5d525c4dde0e616b02821f98a9f54600f49
Rachit <rachitthakur91074@gmail.com>
2025-02-10 07:06:03 +0000

    added hello_chatur

---
+++ hello_chatur.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "chatur: Adarniya sabhapati mahodya"
+
+echo "chatur: aaj hum sab milkar chamatkar karenge"
+
+echo "rancho: ratta marega to aise hi hoga"
+
+echo "raju: rancho itna hi hai tujhe to ek folder bana k dikha"
+
+mkdir -p rancho
+
+echo "rancho: ye le bana dia ro mat ab"
+
+echo "Farhan: virus ne beijati kar di"
+
+echo "Farhan: abba nahi manenge" >  farhanitrate.txt
